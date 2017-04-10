fcf2ea0c47bafff393c327fad76247343ef843a7
Vladislav Savionok <savionak@gmail.com>
2017-04-10 20:39:50 +0300

    Add rectangle grammar
    + update analyze method

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from elements import *
+from elements import Type
+from extended_rules import *
 from syntax_method import analyze
 
-INPUT = [
+INPUT_ROOF = [
     Terminal(Type.DIAGONAL_RIGHT, [0, 0], [10, 10]),
     Terminal(Type.DIAGONAL_LEFT, [10, 10], [20, 0])
 ]
 
+INPUT_RECT = [
+    Terminal(Type.VERTICAL, [0, 0], [0, 10]),
+    Terminal(Type.VERTICAL, [10, 0], [10, 10]),
+    Terminal(Type.HORIZONTAL, [0, 0], [10, 0]),
+    Terminal(Type.HORIZONTAL, [0, 10], [10, 10])
+]
+
+ROOF_GRAMMAR = [
+    RoofRule()
+]
+
+RECT_GRAMMAR = [
+    WallsRule(), BaseRule(), RectRule()
+]
+
 def main():
-    print(analyze(INPUT))
+    print(analyze(INPUT_ROOF, ROOF_GRAMMAR))
+    print(analyze(INPUT_RECT, ROOF_GRAMMAR))
+    print(analyze(INPUT_RECT, RECT_GRAMMAR))
 
 if __name__ == '__main__':
     main()
--- syntax_method.py
+++ syntax_method.py
@@ -1,18 +1,14 @@
-from basic_rules import *
-from extended_rules import *
-
-GRAMMAR = [
-    RoofRule()
-]
-
-def analyze(items_list):
+def analyze(items_list, grammar):
     done = False
     error = False
-    for rule in GRAMMAR:
+    for rule in grammar:
         applied = False
         i = 0
         while not applied and i < len(items_list)-1:
             applied = rule.apply(items_list[i:i+2])
+            if applied:
+                items_list[i] = applied
+                del items_list[i+1]
             i += 1
         error = not applied
     return not error
